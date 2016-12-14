e5a71ab46f85bddfc04c307b6ab9e14a0c40e48f
Chih-Hao <c.yu@garvan.org.au>
2016-12-15 10:45:56 +1100

    (refactor): Made explicit the function in Apis.test.js.

--- src/Apis.test.js
+++ src/Apis.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import FetchMock from 'fetch-mock';
-import * as Utils from './Apis';
+import {getTestList} from './Apis';
 
 describe('Apis', () => {
   it('getTestList list out items', async () => {
     FetchMock.get('*',['Test1','Test2','Test3']);
-    await Utils.getTestList()
+    await getTestList()
       .then((json) => {
         expect(json.length > 0).toEqual(true); 
       });
