02e6fecdc9c3cc8197bb76775e0323aba3fab2c3
Andre Hermanto <a.hermanto@garvan.org.au>
2017-02-08 14:31:38 +1000

    Pretty up navbar

--- src/Containers/UniversalNavigation/index.js
+++ src/Containers/UniversalNavigation/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Nav, Navbar,  NavItem } from 'react-bootstrap';
+import { Nav, Navbar,  NavItem, Grid } from 'react-bootstrap';
+import styled from 'styled-components';
+import logo from '../../assets/images/g1_logo.png';
+
+const Bar = styled(Navbar)`
+  background-color: #e8f0ff !important;
+  background-image: none !important;
+  background-repeat: no-repeat !important;
+`;
+
+const Link = styled(NavItem)`
+  &:hover {
+    background-color: #aabfca !important;
+  }
+`;
+
 export default class UniversalNavigation extends React.Component 
 {    
   render() 
   {  
     return( 
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">Home</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-        </Nav>
-      </Navbar>
+        <Bar>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/"><img src={logo} alt="logo" width="80px" height="30px"/></a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Nav>
+                <Link eventKey={1} href="#">Link</Link>
+                <Link eventKey={2} href="#">Link</Link>
+            </Nav>
+          </Grid>
+        </Bar>
     )
   }
 }
