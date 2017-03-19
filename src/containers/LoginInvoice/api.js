a49f0d9fb3c5b454f8357697bf2f2e51972d793e
Andre Hermanto <a.hermanto@garvan.org.au>
2017-03-20 09:53:29 +1000

    Update API

--- src/containers/LoginInvoice/api.js
+++ src/containers/LoginInvoice/api.js
@@ -2,7 +2,7 @@
  * Returns an invoice. 
  */
 export function getInvoice(user) {
-  return fetch(`${process.env.REACT_APP_BASE_URL}/test-requests?id=${user.id}`)
+  return fetch(`${process.env.REACT_APP_BASE_URL}/test-requests?id=${user.id}&lastName=${user.lastName}&phone=${user.phone}`)
     .then((response) => response.json())
     .then((json) => json.data);
 }
